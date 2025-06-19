0485461f24b5225533b390cd103a1a479238e48d
SDJoeKing <linghao.zhou@outlook.com>
2025-06-19 12:27:02 +0100

    Add files via upload
    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,120 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "tmath.h"
+#include <QDebug>
+#include <QFile>
+#include <QVector3D>
+#include <iostream>
+
+using namespace MATH;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(this, &MainWindow::settingChanged, this, &MainWindow::calculation);
+
+    m_plot = new QCustomPlot(this);
+    QCPColorMap *_map = new QCPColorMap(m_plot->xAxis, m_plot->yAxis);
+    ui->tfmArea->addWidget(m_plot);
+
+    m_plot->xAxis->setLabel("TFM X Axis");
+    m_plot->yAxis->setLabel("TFM Y Axis");
+
+    QCPColorScale *scale = new QCPColorScale(m_plot);
+    _map->setColorScale(scale);
+    // ui->recalculate->click();
+
+
+
+}
+
+
+
+void MainWindow::on_recalculate_clicked()
+{
+    samplingRate = ui->rate->value() * 1e6;
+    speed = ui->speed->value();
+    channels = ui->channel->value();
+    pitch = ui->pitch->value();
+    samples = ui->samples->value();
+    offsexX = ui->offsetX->value();
+    offsetY = ui->offsetY->value();
+    length = ui->width->value();
+    height = ui->height->value();
+    resolution = ui->resolution->value();
+
+    if(!tfm_data.empty())
+        tfm_data.clear();
+    if(!lookUpTable.empty())
+        lookUpTable.clear();
+
+    int numOfRow = height / resolution;
+    int numOfCol = length / resolution;
+
+    // VectorXf probe = VectorXf::LinSpaced(channels, 0, (channels - 1)*pitch);
+
+    for(int tx = 0 ; tx < channels; tx++)
+    {
+        tfm_data.emplace_back(ArrayXXf::Zero(channels, samples));
+        lookUpTable.emplace_back(ArrayXXf::Zero(numOfRow, numOfCol));
+    }
+
+
+
+    // bool out = populateMAT(tfm_data, "../../data.dat");
+
+    bool out = populateMAT(tfm_data, ui->lineEdit->text());
+    if(!out)
+        throw std::runtime_error("Cannot load data");
+    auto map = static_cast<QCPColorMap *>(m_plot->plottable());
+    map->data()->setSize( int(length / resolution), int(height / resolution));
+    map->data()->setRange(QCPRange(offsexX, offsexX+length), QCPRange(offsetY, offsetY + height));
+    map->setGradient(QCPColorGradient::gpJet);
+
+    map->rescaleDataRange();
+    map->rescaleAxes();
+    m_plot->replot(QCustomPlot::rpImmediateRefresh);
+
+    emit settingChanged();
+}
+
+void MainWindow::calculation()
+{
+
+    int numOfRow = height / resolution;
+    int numOfCol = length / resolution;
+
+    generateLookTable(lookUpTable, speed, pitch, offsexX, offsetY, resolution);
+    tfm_result = ArrayXXf(numOfRow, numOfCol);
+
+    // std::cout << tfm_data[0].row(0) << std::endl;
+
+    TFM(tfm_data, tfm_result, lookUpTable, samplingRate);
+
+    QCPColorMap *_map = static_cast<QCPColorMap *>(m_plot->plottable());
+    auto key = _map->data()->keySize();
+    auto value = _map->data()->valueSize();
+
+    _map->data()->fill(0);
+
+    for(int row = 0; row< value; row++)
+    {
+        for(int col = 0; col < key; col++)
+            _map->data()->setCell(col, row, qAbs(tfm_result(row, col)));
+    }
+    _map->setGradient(QCPColorGradient::gpJet);
+    _map->rescaleDataRange();
+    _map->rescaleAxes();
+    m_plot->replot(QCustomPlot::rpQueuedRefresh);
+}
+
+
+
+void MainWindow::on_recalculate_2_clicked()
+{
+    QString name = QFileDialog::getOpenFileName(this, "Select FMC data", QApplication::applicationFilePath(), "*.dat");
+    ui->lineEdit->setText(name);
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "tmath.h"
+#include "qcustomplot.h"
+#include "ui_mainwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow(){delete ui;};
+
+signals:
+    void settingChanged();
+
+private slots:
+    void on_recalculate_clicked();
+    void calculation();
+
+
+    void on_recalculate_2_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    float samplingRate;
+    float speed;
+    int channels;
+    float pitch;
+    int samples;
+    float offsexX;
+    float offsetY;
+    float length;
+    float height;
+    float resolution;
+    std::vector<ArrayXXf> tfm_data;
+    std::vector<ArrayXXf>  lookUpTable;
+    ArrayXXf tfm_result;
+    QCustomPlot *m_plot;
+};
+#endif // MAINWINDOW_H
